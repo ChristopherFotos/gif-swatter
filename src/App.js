3cb85b8778039de229fea7e380d70b196eadb166
Chris Johns <chrisfotosmusic@gmail.com>
2021-01-07 22:49:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Target from './Target'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  constructor(){
+    super()
+    this.targets = [
+      {x: 100, y: 400, velocity: {x:3, y:4}, ref:React.createRef(), friction: 0.97},
+      {x: 150, y: 90 , velocity: {x:1, y:5}, ref:React.createRef(), friction: 0.97},
+      {x: 400, y: 100, velocity: {x:3, y:5}, ref:React.createRef(), friction: 0.97},
+      {x: 100, y: 550, velocity: {x:7, y:3}, ref:React.createRef(), friction: 0.97}
+    ]
+    this.step = this.step.bind(this)
+  }
+
+  step(){  
+  
+    this.targets.forEach( t => {
+        t.x += t.velocity.x
+        t.y += t.velocity.y
+
+        t.velocity.x *= t.friction
+        t.velocity.y *= t.friction
+
+        if(t.ref.current){
+          t.ref.current.style.left = t.x + 'px'
+          t.ref.current.style.top  = t.y + 'px'
+        }
+      }
+    )
+
+    requestAnimationFrame(()=>this.step())
+  }
+
+  start(){
+    let bound = this.step.bind(this)
+    requestAnimationFrame(bound)
+  }
+
+  render() {
+
+    window.addEventListener('click', ()=>{this.start()})
+
+    return (  
+       <div id="id">
+          {this.targets.map(t=> {
+            let style = {
+              position: 'absolute',
+              left: t.x + 'px',
+              top : t.y + 'px'
+            }
+            return <Target reference = {t.ref} style={style}></Target>}
+          )}
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Target.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function Target(props) {
+    return (
+       <h1 style = {props.style} ref={props.reference}>HELLO</h1>
+    )
+}
